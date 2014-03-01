0c1d573df9664837e3f98c9e366776424a44e067
cucuandreea <mad_and_93@yahoo.com>
2014-03-01 10:16:07 +0200

    Gata!! :D

--- first_frame.py
+++ first_frame.py
@@ -2,7 +2,7 @@ import wx
 
 class fr(wx.Frame):
 	def __init__(self,parent,id):
-		wx.Frame.__init__(self,parent,id,'IULIA',size=(300,200))
+		wx.Frame.__init__(self,parent,id,'CUCU',size=(300,200))
 		
 if __name__ == '__main__':
 	app=wx.PySimpleApp()
